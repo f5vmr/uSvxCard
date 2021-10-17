75c9287372dbd4af842c1b80e5c84183f9894a2f
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2021-10-17 17:10:30 +0100

    Update svxcontrol.sh

--- svxcontrol.sh
+++ svxcontrol.sh
@@ -4,10 +4,10 @@ test1=$(grep -c "\#\* \* \* \* \* root pgrep svxlink" /etc/crontab)
 
   if [ $test1 = 0 ]
   then
-   echo "Ajout du #"
+   echo "Add the #"
    sed -i '/root pgrep svxlink/ s/^/#/' /etc/crontab && service cron restart && pkill svxlink
  else 
 #  then
-   echo "Enleve le #"
+   echo "Remove the #"
    sed -i '/root pgrep svxlink/s/^#//' /etc/crontab && service cron restart && /etc/spotnik/restart
 fi
